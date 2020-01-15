e99349028bd4a209a8c1da99bfee120d828211a7
Nico <extra@192.168.100.103>
2020-01-15 14:40:10 -0600

    mongoDb

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); //this loads the defined variables from .env
 
 const config = {
-    mongoDB: process.env.mongoDB + process.env.mongoEnd
+    mongoDB: process.env.mongoDB
 };
 
 module.exports = config;
